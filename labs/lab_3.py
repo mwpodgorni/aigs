3ffb5d45eaed593c345ce287b60ce8f63bfb6258
Noah Syrkis <noah@syrkis.com>
2024-09-02 22:09:16 +0200

    clean lab 3

--- labs/lab_3.py
+++ labs/lab_3.py
@@ -10,15 +10,17 @@ from jaxtyping import Array
 import chex
 from typing import Callable, Dict, List
 import sklearn.datasets as skd
+import os
 import tensorflow_datasets as tfds
+import tensorflow as tf
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# %%
-mnist = tfds.load("mnist", split="train")
-x_data = jnp.array([x["image"] for x in tfds.as_numpy(mnist)]).reshape(-1, 1, 28, 28).astype(jnp.float32)
-y_data = jnp.array([x["label"] for x in tfds.as_numpy(mnist)])
 
+# %% Constants
+batch_size = 32
+
+# %% Load celebA dataset
 # %% Convolutional neural networks ######################################
 # Make a CNN classifying faces using JAX or Equinox.
 # Compare the performance of your model to a simple neural network.
@@ -26,63 +28,90 @@ y_data = jnp.array([x["label"] for x in tfds.as_numpy(mnist)])
 # 2. MAKE AN APPLY FUNCTION THAT TAKES IN THE PARAMS AND INPUTS AND RETURNS THE OUTPUT
 # NOTE: DO BOTH THE CONV STUFF, AND THE FULLY CONNECTED STUFF. REMEMBER ACTIVATION FUNCTIONS.
 # 3. AT THIS POINT YOU HAVE AN OUTPUT "Y_HAT" FROM THE MODEL. USE GRADS TO OPTIMIZE WEIGHTS.
-def conv(input, kernel, stride=1):
+
+# %% Load data
+mnist = tfds.load('mnist', split='train')
+x_data = jnp.array([x['image'] for x in tfds.as_numpy(mnist)]).astype(jnp.float32)
+y_data = jnp.array([x['label'] for x in tfds.as_numpy(mnist)]).astype(jnp.int32)
+
+# %%  Reshape data to baches
+x_data = x_data.reshape(-1, batch_size, 1, 28, 28) / 255.0  # <- normalize
+y_data = y_data.reshape(-1, batch_size)
+
+# %% Define convolutional functions
+def conv(input: Array, kernel: Array, stride: int = 1) -> Array:
     return lax.conv(input, kernel, (stride, stride), padding='SAME')
 
-def init_fn(rng, cfg=None):
+
+# %% Define the model init
+def init_fn(rng, cfg={"fc": [1568, 128, 10]}):
+    """
+    Initialize the parameters of the model in any pytree (list, tuple, dict, etc.) structure you like.
+    We are using dictionaries here. cfg is a dictionary that contains (some) configuration of the model.
+    Better to use cfg than hardcoding the shapes.
+    """
     rng, *keys = random.split(rng, 10)
     kernel_1 = random.normal(keys[0], (3, 1, 3, 3))
     kernel_2 = random.normal(keys[1], (2, 3, 3, 3))
-    w_1 = random.normal(keys[2], (1568, 128))
-    b_1 = random.normal(keys[3], (128,))
-    w_2 = random.normal(keys[4], (128, 10))
-    b_2 = random.normal(keys[5], (10,))
-    params = dict(kernel1=kernel_1, kernel2=kernel_2, w1=w_1, b1=b_1, w2=w_2, b2=b_2)
+    w1 = random.normal(keys[2], (cfg["fc"][0], cfg["fc"][1]))
+    b1 = jnp.zeros(cfg["fc"][1])
+    w2 = random.normal(keys[3], (cfg["fc"][1], cfg["fc"][2]))
+    b2 = jnp.zeros(cfg["fc"][2])
+    params = dict(kernel1=kernel_1, kernel2=kernel_2, mlp=[(w1, b1), (w2, b2)])
     return params
 
+
+# %% Define the model apply
 def apply_fn(params, x):
+    """
+    Apply the model to the input x using the parameters params.
+    We thus go through the forward pass of the model, step by step.
+    We can do so however we like, as long as we are consistent with the shapes.
+    Here I do a few things explicitly, like reshaping the output of the second convolution.
+    I loop through the MLP layers, but you could do this however you like.
+    REMEMBER: this function will be used in the loss function, which we will differentiate with grad.
+    This means JAX can take derivatives of functions with loops!
+    """
     z = conv(x, params['kernel1'])
     z = jnp.tanh(z)
     z = conv(z, params['kernel2'])
-    z = z.reshape(60000, -1)
-    z = z @ params['w1'] + params['b1']
-    z = jnp.tanh(z)
-    z = z @ params['w2'] + params['b2']
+    z = z.reshape(z.shape[0], -1)
+    for w, b in params['mlp']:
+        z = jnp.dot(z, w) + b
+        z = jnp.tanh(z)
     return nn.softmax(z)
 
+# %% Define the loss function
 def loss_fn(params, x_data, y_data):  # y: n
+    """
+    Compute the loss of the model on the input data x_data with labels y_data.
+    We use the mean squared error loss here, but you can use any loss you like.
+    We also one-hot encode the labels y_data, as we are doing multiclass classification.
+    """
     y_hat = apply_fn(params, x_data)
     y_data = jnp.eye(10)[y_data]  # <- one-hot encode  # y: n x 10
     loss = jnp.mean((y_hat - y_data) ** 2)
     return loss
 
-
-
-
-# %%
+# %% Call it all
 rng = random.PRNGKey(0)
 params = init_fn(rng)
 
 grad_fn = jit(grad(loss_fn))
-for i in tqdm(range(100)):
-    grads = grad_fn(params, x_data, y_data)
-    params = tree.map(lambda p, g: p - 0.01 * g, params, grads)  # <- same as above
-#
-# %%
-# %%
-fig, axes = plt.subplots(1, 5, figsize=(12, 4))
-axes[0].imshow(x_data[0, 0], cmap='gray')
-axes[1].imshow(output[0, 0], cmap='gray')
-axes[2].imshow(output[0, 1], cmap='gray')
-axes[3].imshow(output[0, 2], cmap='gray')
-axes[4].imshow(output[0, 3], cmap='gray')
-
+for epoch in range(5): # <- epochs
+    for x, y in tqdm(zip(x_data, y_data), total=len(x_data)):  # <- batches
+        grads = grad_fn(params, x, y)  # <- compute gradients
+        params = tree.map(lambda p, g: p - 0.01 * g, params, grads)  # <- update parameters
 
 
 
 # %% Autoencoders #######################################################
 # Implement an autoencoder using JAX or Equinox.
 # Look at the reconstruction error and the latent space.
+
+def deconv(input: Array, kernel: Array, stride: int = 1) -> Array:  # <- transpose convolution (deconvolution) won't use for now
+    return lax.conv_transpose(input, kernel, (stride, stride), padding='SAME')
+
 
 
 # %% Variational Autoencoders ###########################################
--- ludens.py
+++ ludens.py
@@ -1,7 +1,20 @@
 # %% Imports
-from jax import random, grad, tree
-import jax.numpy as jnp
+import jax.numpy as jnp  # <- the core of jax are jnp.ndarrays
+from jax import jit, grad, random
+
+# %% ludens
+jnp.array([1, 2, 3])  # <- jnp.array
 
 
-def identity(x):
-    return x
+def f(x):
+    return x ** 2
+
+
+x = jnp.array(3.0)  # <- jnp.array
+f(x), grad(f)(x)  # <- jnp.array
+
+
+#########################################################################
+
+rngs = random.split(random.PRNGKey(0), 10)
+fs = [f(rng) for rng in rngs]  # <- list comprehension
