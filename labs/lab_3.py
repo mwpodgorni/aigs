f544de4e09c2bb49250344377d5e1ee5de47954b
Noah Syrkis <noah@syrkis.com>
2024-09-02 12:54:57 +0200

    clean

--- labs/lab_3.py
+++ labs/lab_3.py
@@ -3,22 +3,40 @@
 # by: Noah Syrkis
 
 # %% Imports ############################################################
-from jax import grad
+from jax import grad, lax, random
 import jax.numpy as jnp
 from jaxtyping import Array
 import chex
 from typing import Callable, Dict, List
-import equinox as eqx
 import sklearn.datasets as skd
+import tensorflow_datasets as tfds
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # %%
-faces = skd.fetch_lfw_people()
-data = jnp.array(faces.data)  # type: ignore
-target = jnp.array(faces.target)  # type: ignore
+mnist = tfds.load("mnist", split="train")
+x_data = jnp.array([x["image"] for x in tfds.as_numpy(mnist)]).reshape(-1, 1, 28, 28)
+y_data = jnp.array([x["label"] for x in tfds.as_numpy(mnist)])
 
 # %% Convolutional neural networks ######################################
 # Make a CNN classifying faces using JAX or Equinox.
 # Compare the performance of your model to a simple neural network.
+def conv(input, kernel):
+    return lax.conv(input, kernel, (1, 1), padding='SAME')
+
+rng = random.PRNGKey(0)
+# 1 channel to 4 channels
+kernel = random.normal(rng, (4, 1, 3, 3))  # <- 3x3 kernel, 1 input channel, 4 output channels
+output = conv(x_data, kernel)  # <- x.data.shape = (n, 1, 28, 28)
+
+# %%
+fig, axes = plt.subplots(1, 5, figsize=(12, 4))
+axes[0].imshow(x_data[0, 0], cmap='gray')
+axes[1].imshow(output[0, 0], cmap='gray')
+axes[2].imshow(output[0, 1], cmap='gray')
+axes[3].imshow(output[0, 2], cmap='gray')
+axes[4].imshow(output[0, 3], cmap='gray')
+
 
 
 # %% Autoencoders #######################################################
