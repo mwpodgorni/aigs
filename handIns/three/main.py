0befe4a5163ce4b6afcfacfa7ccca30f3478724c
mwpodgorni <mwpodgorni@gmail.com>
2024-10-07 18:39:30 +0200

    dataset

--- handIns/three/main.py
+++ handIns/three/main.py
@@ -1,4 +1,5 @@
-# Parabellum based miniproject
+# lax.map(partial(apply_kernel, x[0]), kernels) # -> what you want to plot
 
 
-lax.map(partial(apply_kernel, x[0]), kernels) # -> what you want to plot
+import jax
+import optax
